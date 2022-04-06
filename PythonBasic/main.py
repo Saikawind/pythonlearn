401887e7490e3a7ff4db6b4e1cb6a94cf1592908
Saika <834772398@qq.com>
2022-04-07 02:09:07 +0800

    add some notebook

--- PythonBasic/main.py
+++ PythonBasic/main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+# 参考：https://itqna.net/questions/37569/what-print-flush-python-closed
+import time
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+for _ in range(5):
+    print('.', end='')
+    time.sleep(0.5)
+print('  Gairuo!')
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hello, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# 在终端中最后一次性显示
+# ..... Gairuo!
